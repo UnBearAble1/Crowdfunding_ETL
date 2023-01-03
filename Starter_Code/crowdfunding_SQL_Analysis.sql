a206f61a4a62d1018a1be6b73b18dc0f2958dd2e
UnBearAble1 <jeremiah.slinde@yahoo.com>
2023-01-03 16:34:58 -0600

    Updated Tables for backers and email lists, added SQL query

---
+++ Starter_Code/crowdfunding_SQL_Analysis.sql
@@ -0,0 +1,72 @@
+-- Challenge Bonus queries.
+-- 1. (2.5 pts)
+-- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
+SELECT c.backers_count,
+c.cf_id
+INTO live_backers
+FROM campaign AS c
+WHERE (c.outcome = 'live')
+ORDER BY (c.backers_count) DESC;
+
+
+-- 2. (2.5 pts)
+-- Using the "backers" table confirm the results in the first query.
+SELECT COUNT (b.cf_id), b.cf_id
+INTO backers_check
+FROM backers AS b
+GROUP BY b.cf_id
+ORDER BY COUNT (b.cf_id) DESC;
+
+
+-- 3. (5 pts)
+-- Create a table that has the first and last name, and email address of each contact.
+-- and the amount left to reach the goal for all "live" projects in descending order. 
+
+-- update campaign table with new column for remaining amount
+ALTER TABLE campaign ADD COLUMN "Remaining Goal Amount" int GENERATED ALWAYS AS (goal - pledged) STORED;
+
+SELECT cl. first_name,
+cl.last_name,
+cl.email,
+c."Remaining Goal Amount"
+INTO email_contact_remaining_goal_amount
+FROM contacts AS cl
+INNER JOIN campaign AS c
+ON (cl.contact_id = c.contact_id)
+WHERE (c.outcome = 'live')
+ORDER BY c."Remaining Goal Amount" DESC;
+
+-- Check the table
+SELECT * FROM email_contact_remaining_goal_amount;
+
+
+-- 4. (5 pts)
+-- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
+-- and has the first and last name of each backer, the cf_id, company name, description, 
+-- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
+
+-- Rename Column ot meet request criter
+ALTER TABLE campaign RENAME COLUMN "Remaining Goal Amount" TO "Left of Goal";
+
+SELECT b.email,
+b.first_name,
+b.last_name,
+b.cf_id,
+c.company_name,
+c.description,
+c.end_date,
+c."Left of Goal"
+INTO email_backers_remaining_goal_amount
+FROM backers AS b
+INNER JOIN campaign AS c
+ON (b.cf_id = c.cf_id)
+WHERE (c.outcome = 'live')
+ORDER BY b.last_name;
+-- Check the table
+SELECT * FROM email_backers_remaining_goal_amount;
+
+
+
+-- Check the table
+
+
--- Starter_Code/crowdfunding_db_schema.sql
+++ Starter_Code/crowdfunding_db_schema.sql
@@ -1,4 +1,8 @@
-﻿CREATE TABLE "campaign" (
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
     "company_name" varchar(100)   NOT NULL,
@@ -49,11 +53,13 @@ CREATE TABLE "backers" (
     "cf_id" int   NOT NULL,
     "first_name" varchar(50)   NOT NULL,
     "last_name" varchar(50)   NOT NULL,
-    "email" varchar(100)   NOT NULL,
     CONSTRAINT "pk_backers" PRIMARY KEY (
-        "backer_id"
+        "cf_id"
      )
 );
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "backers" ("cf_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "contacts" ("contact_id");
@@ -63,7 +69,4 @@ REFERENCES "category" ("category_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
-
-ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
-REFERENCES "campaign" ("cf_id");
 
